1ea7f9f2bb02681986f054ab85750c6323a58924
Benjamin-Threlkeld <benjamin.threlkeld@gmail.com>
2013-11-03 23:29:41 -0600

    Updated template and cleaned up code a bit, more can definitely be done to it.

--- source/javascripts/getJson.js
+++ source/javascripts/getJson.js
@@ -1,12 +1,15 @@
 var activeAjaxConnections = 0;
-var myObject = {};
+var eventsObject = {};
+var eventsObjectKeys = [];
 
-makeRequest = function (Fname) {
-	//connection opened
+makeRequest = function (dObj) {
+  var fileName = (dObj.getDate()) + '.' + (dObj.getMonth() + 1) + '.' + (dObj.getFullYear() - 2000);
+	console.log("requesting file " + fileName + ".json");
+  //connection opened
 	activeAjaxConnections++;
 	//console.log("activeAjaxConnections: " + activeAjaxConnections + " on opened");
 	//new xmlhttprequest
-	var ai = new AJAXInteraction("/events/" + Fname + ".json", function(req) {
+	var ai = new AJAXInteraction("/events/" + fileName + ".json", function(req) {
 		//connection closed
 
 		//if the headers were returned
@@ -27,16 +30,24 @@ makeRequest = function (Fname) {
 			//if it is ok or hasn't been modified (cached)
 			if (req.status === 200 || req.status === 304) {
 				//console.log("good");
-				myObject[Fname] = JSON.parse(req.responseText);
-			} else if (req.status === 404) {
-				//console.log("not found");
-			}
-			//console.log(req.responseText);
-			//console.log(activeAjaxConnections); //if this is enabled, it calls parseEvents() twice, perhaps due to something being there for it check how many connections are available  
-			// if all requests have returned
-			if (activeAjaxConnections === 0) {
-				console.log('parsing Events'); //this returns twice and I don't know how to fix it
-				parseEvents(myObject);
+				eventsObject[fileName] = JSON.parse(req.responseText);
+        datesOfEventsLen = eventsObjectKeys.length;
+        console.log("length " + datesOfEventsLen);
+        eventsObjectKeys[datesOfEventsLen] = fileName;
+        
+        //console.log("date day " + date.getDate());
+        //console.log((dObj.getDate()) + '.' + (dObj.getMonth() + 1) + '.' + (dObj.getFullYear() - 2000));
+        //console.log("date day: " + datesOfEvents[datesOfEventsLen + 1].getMonth());
+      } else if (req.status === 404) {
+        //console.log("not found");
+      }
+      //console.log(req.responseText);
+      //console.log(activeAjaxConnections); //if this is enabled, it calls parseEvents() twice, perhaps due to something being there for it check how many connections are available  
+      // if all requests have returned
+      if (activeAjaxConnections === 0) {
+        console.log('parsing Events'); //this returns twice sometimes and I don't know how to fix it.
+        //console.log(eventsObjectKeys);
+        parseEvents(eventsObject, eventsObjectKeys);
 			}
 		}
 		//console.log(activeAjaxConnections);
@@ -47,7 +58,6 @@ makeRequest = function (Fname) {
 
 getEventFiles = function(fromToday, daysToLookAhead, today) {
 	
-	// set defult values
 	// 1 would start from today, 2 would start from tomorrow
 	fromToday = (typeof fromToday !== 'undefined' ? fromToday : 1);
 	daysToLookAhead = (typeof daysToLookAhead !== 'undefined' ? daysToLookAhead : 3);
@@ -58,19 +68,22 @@ getEventFiles = function(fromToday, daysToLookAhead, today) {
 	if (typeof today !== 'undefined') {
 		daysLater = new Date();
     console.log("Manual start day: " + today);
-    date = today.split('.'); // 20.10.13
     date = {
-      "getDate": parseInt(date[0], 10) - 1, //everything starts at 0
-      "getMonth": parseInt(date[1], 10) - 1, //0 for January
-      "getFullYear": 2000 + parseInt(date[2], 10) // 2000+13 <- glad this one doesn't, oh wait, it does 0 is for no years
+      "getDate": today[0] - 1, //everything starts at 0
+      "getMonth": today[1] - 1, //0 for January
+      "getFullYear": 2000 + today[2] // 2000+13 <- glad this one doesn't, oh wait, it does 0 is for no years
     };
     //daysLater.setDate(date.getDate);
     daysLater.setMonth(date.getMonth, date.getDate);
     daysLater.setFullYear(date.getFullYear);
   };
-
+  //var thing = {"this": {"is": {"an":["o","b","j","e","c","t"]}}};
   //get event files fromToday offset
   for (var i = fromToday; i <= totalDays; i++) {
+    var thing = new Date();
+    thing.setDate(1);
+    thing.setMonth(1);
+    thing.setFullYear(2018);
     if (typeof today === 'undefined') {
     	daysLater = new Date();
     } else {
@@ -79,10 +92,11 @@ getEventFiles = function(fromToday, daysToLookAhead, today) {
     }
 		//add `i` day(s) to date
 		daysLater.setDate(daysLater.getDate() + i);
+    //console.log(daysLater.getDate());
 		//string this date in dd.mm.yy form
-		S_thisDay = daysLater.getDate() + "." + (daysLater.getMonth() + 1) + "." + daysLater.getFullYear().toString().substring(2, 4);
-		console.log("requesting file " + S_thisDay + ".json");
-		makeRequest(S_thisDay);
+		//S_thisDay = daysLater.getDate() + "." + (daysLater.getMonth() + 1) + "." + daysLater.getFullYear().toString().substring(2, 4);
+		//console.log("requesting file " + S_thisDay + ".json");
+		makeRequest(daysLater, thing);
 	}
 
 };
--- source/javascripts/parseEvents.js
+++ source/javascripts/parseEvents.js
@@ -1,60 +1,134 @@
+
+eventDateFromString = function (dateString) {
+  d = new Date();
+  date = dateString.split('.');
+  d.setDate(parseInt(date[0], 10) - 1);
+  d.setMonth(parseInt(date[1], 10) - 1);
+  d.setFullYear(parseInt(date[2], 10) + 2000);
+  return d;
+}
+
+timeFromString = function (timeString) {
+  // set new date object
+  var d = new Date();
+  // match |`digit`|:|`digit``digit`|`whitespace`|`p`|case insensitive
+  var timeParsed = timeString.match(/(\d+)(?::(\d\d))?\s*(p?)/i);
+  
+  timeParsed[1] = parseInt(timeParsed[1], 10);
+  timeParsed[2] = parseInt(timeParsed[2], 10);
+  // time Hour, length of string
+  var timeParsed1String = timeParsed[1] + '';
+  var timeParsed1Len = timeParsed1String.length;
+  // if p does not exist and the length of the time is 4 or 3
+  if (!timeParsed[3] && (timeParsed1Len === 4 || timeParsed1Len === 3)) {
+    // cut form the end of timeStr
+    d.setHours(parseInt(timeParsed1String.substr(0, timeParsed1Len - 2), 10));
+    d.setMinutes(parseInt(timeParsed1String.substr(timeParsed1Len - 2), 10) || 0);
+    // if minutes
+  } else if (timeParsed[2]) {
+    // set hour without adding 12
+    d.setHours(timeParsed[1]);
+    d.setMinutes(timeParsed[2] || 0);
+    // if p and hour is not 12 
+  } else if (timeParsed[3] && (timeParsed[1] !== 12)) {
+    // set hour, adding 12
+    d.setHours(timeParsed[1] += 12);
+    d.setMinutes(timeParsed[2] || 0);
+  }
+  return d;
+}
+
+timeTo12Hour = function(dObj) {
+  var hours = dObj.getHours();
+  var minutes = dObj.getMinutes();
+  var d12Hour;
+  if (hours > 12) {
+    d12Hour = {
+      "getHour": hours - 12,
+      "getMinute": minutes,
+      "getPeriod": "pm"
+    } 
+  } else {
+    d12Hour = {
+      "getHour": hours,
+      "getMinute": minutes,
+      "getPeriod": "am"
+    }
+  }
+  return d12Hour;
+}
+
 updateCalendar = function (daysOfEvents) {
   var calendars = document.querySelectorAll('section.calendar ul.events');
   //update calendar 0
   calendars[0].innerHTML = daysOfEvents;
 }
 
-parseEvents = function (events) {
+parseEvents = function (events, eventkeys) {
   // date formatting arrays
   var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
       weekdays_abb = ["Sun","Mon","Tues","Weds","Thurs","Fri","Sat"],
       months = ["January","February","March","April","May","June","July","August","September","October","November","December"],
       months_abb = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
   /* ------TODO------
-  // - Sort object by date (day) and by time. Passing the date as an object will help with that too
+  // - Sort object by date (day) and by time. Passing the dates as an object will help with that too
   // - Prob want to make it only create one day of events at a time. This will most likely bog down
-  // - Make getEventFiles() accept a dev argument for the start date so the files mustn't renamed every couple days
-  // - Pass dates parseEvent() in an array so we don't waste resource parsing a one date straight back to a date
+  // - Make getEventFiles() accept a dev argument for the start date so the files mustn't renamed every couple days < done
+  // - Pass dates to parseEvent() in an array so we don't waste resource parsing a once date straight back to a date < 
+  //     prob wont be worth the trouble, because it would require knowing if the file returned or not. 
+  //     hey i just thought of something, perhaps if in the getJson.js > makeRequest(), it already passes the pretty date, 
+  //     I could just move the pretty date var into the function and pass the whole date object, and when the requested file is added to the 
+  //     object of events just add the date to the dateOfEvents object. < won't be easy, I thought of the fact that the async XHRs will not return in order and the dates were only the last date, so the date object was updating all instances of the date.
   // --More to come--
   */
-  var eventDays = Object.getOwnPropertyNames(events),
-      daysCount = eventDays.length,
-      //dayOfEvents_HTML = [],
-      daysOfEvents_HTML = "";
-      //console.log(daysCount);
+
+  var daysCount = eventkeys.length,
+  daysOfEvents_HTML = "";
+  //console.log("daysCount" + daysCount);
+  //console.log(eventkeys);
+  console.log(eventkeys);
+  var dasdd = new Date();
+  console.log(dasdd.toString());
+  //console.log(dates[0].getDate() + '.' + (dates[0].getMonth() + 1) + '.' + (dates[0].getFullYear() - 2000));
 
   for (var i = 0; i < daysCount; i++) {
+    //console.log(eventKeys);
 
-    var date = eventDays[i].split('.'); // 20.10.13
+    var date = eventDateFromString(eventkeys[i]);
+    var eventsDateH2 = (months[date.getMonth()]) + ' ' + (date.getDate() + 1) + ', ' + (date.getFullYear() - 2000);
+    var event_ul_id = (date.getFullYear() - 2000) + '-' + (date.getMonth() - 1) + '-' + (date.getDate() + 1);
+    //event_ul_id = (dates.getMonth() - 1)+'-'+date.getDate+'-'+date.getFullYear;
     
-    date = {
-      "getDate": date[0],
-      "getMonth": date[1],
-      "getFullYear": 20 + date[2] // 20<2digitYear>
-    };
-
-    thisDayEventPrettyDate = date.getMonth+'-'+date.getDate+'-'+date.getFullYear;
-    //dayOfEvents_HTML[thisDayEventPrettyDate] = [];
-    /* some stuff for a more advanced date
-    var d = new Date();
-    d.setDate(parseInt(date.getDate,10));
-    d.setMonth(parseInt(date.getMonth - 1,10)); // js month starts at 0 humans start at 1
-    d.setFullYear(2000 + parseInt(date[2],10)); //faster to add numbers than concat strings and sense date object requires an int than it may as well be parsed first
-    console.log(months[d.getMonth()] + ' ' + weekdays[d.getDay()] + ', ' + d.getFullYear());*/
-    var prettyDate = date.getDate + ' ' + months[date.getMonth] + ', ' + date.getFullYear;
-    
-    // If we want much more templating then this we should proby use pre compiled Handlebars
+    // If we want much more complex templating then this we should probly use pre compiled Handlebars or something similar
     daysOfEvents_HTML +=
-    '<li class="date">' + prettyDate + '\n' +
-    '  <ul id="' + thisDayEventPrettyDate + '">\n';
+    '<li class="date">\n' + 
+    '  <h2>' + eventsDateH2 + '</h2>\n' +
+    '  <ul id="' + event_ul_id + '">\n';
 
-    for (var i2 = 0; i2 <= events[eventDays[i]].length - 1; i2++) {
+    for (var i2 = 0, l2 = events[eventkeys[i]].length; i2 <= l2 - 1; i2++) {
       //te = thisEvent
-      var te = events[eventDays[i]][i2];
-
+      var te = events[eventkeys[i]][i2];
+      // parse the time string to date object, which the hour is 24 hour, then transform the date object to 12 hour object (not date Object)
+      var eventStartTime = timeTo12Hour(timeFromString(te.startTime));
+      var eventEndTime = timeTo12Hour(timeFromString(te.endTime));
+      //console.log(eventTimeStart);
+      var prettyEventStartTime =
+      '<time>\n' +
+      '  <span class="hour">' + eventStartTime.getHour + '</span>\n' +
+      '  <span class="minute">' + eventStartTime.getMinute + '</span>\n' +
+      '  <span class="daypart ' + eventStartTime.getPeriod + '">' + eventStartTime.getPeriod + '</span>\n' +
+      '</time>\n';
+      
+      var prettyEventEndTime =
+      '<time>\n' +
+      '  <span class="hour">' + eventEndTime.getHour + '</span>\n' +
+      '  <span class="minute">' + eventEndTime.getMinute + '</span>\n' +
+      '  <span class="daypart ' + eventEndTime.getPeriod + '">' + eventEndTime.getPeriod + '</span>\n' +
+      '</time>\n';
+      
       daysOfEvents_HTML +=
       '<li class="event">\n'+
-      '  <span class="time">'+te.startTime+'</span>\n'+
+        prettyEventStartTime +
       '  <span class="title">'+te.title+'</span>\n'+
       '  <span class="location">'+te.location.address+'</span>\n'+
       '</li>\n';
@@ -69,7 +143,6 @@ parseEvents = function (events) {
 }
 // 1,0 gets today and 0 days ahead, so 1 file
 // 1,5 gets today and 5 days ahead, so 6 files
-getEventFiles(1,10,"23.10.13");
-//console.log(getEventFiles(1,5));
+getEventFiles(1,5,[23,10,13]);
 // ^ ^ ^ ^ ^ ^ ^- this does everythings!
 
