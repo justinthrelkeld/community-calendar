7210a5c10e0e90b4ef533e26404c908e4645c008
Benjamin-Threlkeld <benjamin.threlkeld@gmail.com>
2014-03-07 23:47:13 -0600

    Fixed where when the popup template was set when the event was clicked. now it does not blink

--- source/javascripts/_maps.js
+++ source/javascripts/_maps.js
@@ -2,13 +2,15 @@ function initMap(geoJson){
   var map = L.mapbox.map('map', 'justinthrelkeld.gi35hf5n');
   map.featureLayer.setGeoJSON(geoJson);
   
+  map.featureLayer.eachLayer(function(marker){
+	  var content = "<h1>" + marker.feature.properties.title + "</h1>" +
+	    "<p>" + marker.feature.properties.description + "</p>";
+	  
+	  marker.bindPopup(content);
+	});
+	
   window.eventClicked = function (e) {
     map.featureLayer.eachLayer(function(marker){
-      var content = "<h1>" + marker.feature.properties.title + "</h1>" +
-        "<p>" + marker.feature.properties.description + "</p>";
-      
-      marker.bindPopup(content);
-
   		if ('e' + marker.feature.properties.hash === e.id) {
   			map.panTo(marker.getLatLng());
   			marker.openPopup();
